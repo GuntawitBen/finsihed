dfe84c75cbcfa5f32490ca07c4d1beae84653396
GuntawitBen <132637794+GuntawitBen@users.noreply.github.com>
2023-07-08 17:09:45 +0000

    fixed

--- stack.h
+++ stack.h
@@ -1,69 +1,72 @@
 #include <iostream>
 #ifndef stack_h
-#define  stack_h
+#define stack_h
 #include "node.h"
-using namespace std;
 
-class Stack{
+class Stack {
 private:
 	NodePtr top;
 	int size;
+
 public:
-  Stack(NodePtr = NULL);
-  ~Stack();
-  char pop();
-  void push(char);
-  bool isEmpty() const;
-  char getTop() const;
-  char getData() const;
+	Stack(NodePtr = NULL);
+	~Stack();
+	char pop();
+	void push(char);
+	bool isEmpty() const;
+	char getTop() const;
+	char getData() const;
 };
 
-Stack::Stack(NodePtr t){
-  if(t) {
-    top=t;
-    size=1;
-  }
- else{
-   top=NULL;
-	 size=0;
-   }
+bool Stack::isEmpty() const {
+  return (top == NULL);
 }
 
-void Stack::push(char x){ 
-   NodePtr new_node=new NODE(x);
-  if(new_node) {
-	 	new_node->set_next(top); //2 Connect to the previous top
-    top = new_node;          //3
-    ++size;                  //4
-   }
-  else cout << "No memory left for new nodes" << endl;
+Stack::Stack(NodePtr t) {
+	if (t) {
+		top = t;
+		size = 1;
+	}
+	else {
+		top = NULL;
+		size = 0;
+	}
 }
 
+void Stack::push(char x) {
+	NodePtr new_node = new NODE(x);
+	if (new_node) {
+		new_node->set_next(top);
+		top = new_node;
+		++size;
+	}
+	else {
+		std::cout << "No memory left for new nodes" << std::endl;
+	}
+}
 
 char Stack::pop() {
- 	 NodePtr t=top;
-	 char value;		
-	 if(t) {
-     value=t->get_value();
-     top=t->get_next();
-	   //finished
-     delete t;
-     --size;
-     return value;
-     }
-	 cout<<"Empty stack"<<endl;
-   return 0;
+	NodePtr t = top;
+	char value;
+	if (t) {
+		value = t->get_value();
+		top = t->get_next();
+		delete t;
+		--size;
+		return value;
 	}
+	std::cout << "Empty stack" << std::endl;
+	return 0;
+}
 
 Stack::~Stack() {
-  cout<<"Clearing all stacks"<<endl;
-  int i;
-  NodePtr t=top;
-  for(i=0;i<size;i++){
-  top = t->get_next();
-  delete t;
-  t = top;
-  }
+	std::cout << "Clearing all stacks" << std::endl;
+	NodePtr t = top;
+	while (t) {
+		top = t->get_next();
+		delete t;
+		t = top;
+	}
 }
 
 #endif
